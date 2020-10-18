bed12dbe02eef65a3bd0f282bcea8c09d398e126
Wentao Wu <wentao3970@gmail.com>
2020-10-18 16:45:56 -0400

    release-1.0

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/bubu/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.bubu.ecommerce.config;
+
+import com.bubu.ecommerce.entity.Product;
+import com.bubu.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable http methods for Product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable http methods for ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+}
